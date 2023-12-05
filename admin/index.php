30c00fe442a7a5e212f01837bb66507c879ccb29
Nithin611 <113677170+Nithin611@users.noreply.github.com>
2023-12-05 22:52:37 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -59,7 +59,7 @@ exit();
 				
 					<ul class="nav pull-right">
 
-						<li><a href="http://localhost/shopping/">
+						<li><a href="https://shopping6.azurewebsites.net/">
 						Back to Portal
 						
 						</a></li>
